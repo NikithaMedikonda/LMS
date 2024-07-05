ee10592e4abf361880e66415225f63a84bdccd9c
Nikitha Medikonda <admin@Nikitha.local>
2024-07-05 22:26:18 +0530

    changes added

--- models/associations.ts
+++ models/associations.ts
@@ -7,19 +7,19 @@ import {Reservations} from './reservations';
 async function getAssociation() {
     
 Authors.hasMany(Books, { foreignKey: 'authorId' });
-Books.belongsTo(Authors, { foreignKey: 'id' });
+Books.belongsTo(Authors, { foreignKey: 'authorId' });
 
-Members.hasMany(Loans, { foreignKey: 'id' });
-Loans.belongsTo(Members, { foreignKey: 'id' });
+Members.hasMany(Loans, { foreignKey: 'member_id' });
+Loans.belongsTo(Members, { foreignKey: 'member_id' });
 
-Books.hasMany(Loans, { foreignKey: 'id' });
-Loans.belongsTo(Books, { foreignKey: 'id' });
+Books.hasMany(Loans, { foreignKey: 'book_id' });
+Loans.belongsTo(Books, { foreignKey: 'book_id' });
 
-Members.hasMany(Reservations, { foreignKey: 'id' });
-Reservations.belongsTo(Members, { foreignKey: 'id' });
+Members.hasMany(Reservations, { foreignKey: 'member_id' });
+Reservations.belongsTo(Members, { foreignKey: 'member_id' });
 
-Books.hasMany(Reservations, { foreignKey: 'id' });
-Reservations.belongsTo(Books, { foreignKey: 'id' });
+Books.hasMany(Reservations, { foreignKey: 'book_id' });
+Reservations.belongsTo(Books, { foreignKey: 'book_id' });
 }
 
 
